79cdcf2acd12d3c1cf259e972f5d457a9849d7aa
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-10-04 19:41:05 +0200

    add ProductRepository

---
+++ src/main/java/com/company/foodorderapp/controller/foodController/BurgerController.java
@@ -0,0 +1,7 @@
+package com.company.foodorderapp.controller.foodController;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BurgerController {
+}
--- src/main/java/com/company/foodorderapp/food/Drink.java
+++ src/main/java/com/company/foodorderapp/food/Drink.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Drink extends Product {
+public class Drink extends Product implements Serializable {
+    private static final long serialVersionUID = 8539936152170847419L;
 
     private String size;
-
 }
