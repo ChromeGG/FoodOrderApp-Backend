21d48dae12acc993110f0517873d5aed3490741b
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-10-09 14:33:55 +0200

    add properties that help debug Hibernate & SQL queries

--- src/main/java/com/company/foodorderapp/FoodOrderAppApplication.java
+++ src/main/java/com/company/foodorderapp/FoodOrderAppApplication.java
@@ -4,6 +4,8 @@ import com.company.foodorderapp.services.DatabaseFillService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FoodOrderAppApplication {
@@ -17,5 +19,10 @@ public class FoodOrderAppApplication {
 
         dataFill.fillDatabase();
     }
+
+//    @Bean
+//    WebMvcConfigurer webMvcConfigurer() {
+//        re
+//    }
 
 }
